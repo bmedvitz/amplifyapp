1aad2d6828cb6e92a0726e1b77965a3cdce08515
bmedvitz <103617166+bmedvitz@users.noreply.github.com>
2022-04-14 05:06:10 -0700

    tic tac toe game 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Game from './Game'
+import Game from './game'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
